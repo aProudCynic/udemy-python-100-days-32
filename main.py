2ab5e43e36993630b8b102c551f12de9495d7134
Balazs Marton <balazs.marton@prezi.com>
2021-07-01 23:46:26 +0200

    feature: add basic functionality

---
+++ main.py
@@ -0,0 +1,12 @@
+import smtplib
+from secrets import (
+    TEST_EMAIL_ACCOUNT_USERNAME,
+    TEST_EMAIL_ACCOUNT_PASSWORD,
+    TARGET_EMAIL_ADDRESS,
+)
+
+connection = smtplib.SMTP(host="smtp.gmail.com", port=587)
+connection.starttls()
+connection.login(user=TEST_EMAIL_ACCOUNT_USERNAME, password=TEST_EMAIL_ACCOUNT_PASSWORD)
+connection.sendmail(from_addr=TEST_EMAIL_ACCOUNT_USERNAME, to_addrs=TARGET_EMAIL_ADDRESS, msg="Hello")
+connection.close()
